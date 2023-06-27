ca937ac88dcbad185fb8181db2e395724529d0e9
Jesteban97 <jearistizabal@i4digital.com>
2023-06-27 09:09:13 -0500

    Ajustes en fakeclass
    Ajustes en fakeclass

--- tests/FakeClass/GenericListRepositoryFake.ts
+++ tests/FakeClass/GenericListRepositoryFake.ts
@@ -2,6 +2,9 @@ import { IGenericList } from "App/Interfaces/GenericListInterfaces";
 import { IGenericListRepository } from "App/Repositories/GenericListRepository";
 
 export class GenericListRepositoryFake implements IGenericListRepository {
+  getGenericListByGroupers(_grouper: string[]): Promise<IGenericList[]> {
+    throw new Error("Method not implemented.");
+  }
   getGenericListByGrouper(_grouper: string): Promise<IGenericList[]> {
     return new Promise((res) => {
       res([]);
